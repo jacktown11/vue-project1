e5937d0dc36654a2f9156402dac1a5d69717188f
jacktown11 <jikai_tang@163.com>
2017-12-08 09:11:31 +0800

    首页部分完成，安装和尝试使用express和vue-router之前

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Layout from './components/layout'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#layout',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
